572ef4f3fcfbefe3d92122db280b10149f55559f
pengpenglang <2300546456@qq.com>
2022-01-13 11:33:53 +0800

    langwenpeng

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 class Calculator:
     name = 'Good calculator'
     price = 18
-     
+
     def add(self, x, y):
         print(self.name)
-        result = x+y
+        result = x + y
         print(result)
 
     def minus(self, x, y):
-        result = x-y
+        result = x / y
         print(result)
 
     def times(self, x, y):
-        print(x*y)
+        print(x * y)
 
-calculator=Calculator()
-calculator.name='hhhh'
-calculator.add(1,2)
+
+calculator = Calculator()
+calculator.name = 'hhhh'
+calculator.add(1, 2)
