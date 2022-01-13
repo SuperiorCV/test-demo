f28f9d1e16a0857abdb1e8749a083f33ec1d6aa8
Langwenchong <1422257646@qq.com>
2022-01-13 11:25:57 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+class Calculator:
+    name = 'Good calculator'
+    price = 18
+     
+    def add(self, x, y):
+        print(self.name)
+        result = x+y
+        print(result)
+
+    def minus(self, x, y):
+        result = x-y
+        print(result)
+
+    def times(self, x, y):
+        print(x*y)
+
+calculator=Calculator()
+calculator.name='hhhh'
+calculator.add(1,2)
